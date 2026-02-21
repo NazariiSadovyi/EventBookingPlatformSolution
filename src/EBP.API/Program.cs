d738663ec5ff53d99912b9c20b26a9e129fe45fd
Nazarii Sadovyi <frost7412359@gmail.com>
2026-02-21 11:56:32 +0200

    Fix account

--- src/EBP.API/Program.cs
+++ src/EBP.API/Program.cs
@@ -16,7 +16,7 @@ namespace EBP.API
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            // Configure the HTTP request pipeline. 
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
